26a56b6f30b1f3cf660d3e0d3ba7359f1b4c5462
Carlos Medina <CARLOSCMEDINA@YAHOO.COM>
2023-01-23 14:01:32 -0600

    Add files via upload

---
+++ Story.cs
@@ -0,0 +1,59 @@
+﻿//Get input from the user
+String noun;
+String adj;
+String noun2;
+String verb;
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Please provide a noun: ");
+        noun = Console.ReadLine();
+        if (noun.Length > 0) { break; }
+    }
+    catch (Exception e) { Console.WriteLine("There was an error" + e); }
+}
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Please provide a adjective: ");
+        adj = Console.ReadLine();
+        if (adj.Length > 0) { break; }
+    }
+    catch (Exception e) { Console.WriteLine("There was an error" + e); }
+}
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Please provide another noun: ");
+        noun2 = Console.ReadLine();
+        if (noun2.Length > 0) { break; }
+    }
+    catch (Exception e) { Console.WriteLine("There was an error" + e); }
+}
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Please provide a verb: ");
+        verb = Console.ReadLine();
+        if (verb.Length > 0) { break; }
+    }
+    catch (Exception e) { Console.WriteLine("There was an error" + e); }
+}
+
+
+String story = "Just then a " + noun +  " rang, and the green girl said to Dorothy, “That is the signal. You must go into the " 
++"Throne Room alone.”\r\n\r\nShe opened a " + adj + " door and Dorothy walked boldly through and found herself in a wonderful" 
++" place. It was a big, round room with a high arched " + noun2 + ", and the walls and ceiling and floor were covered with large" 
++" emeralds set closely together. In the center of the roof was a great light, as bright as the sun, which" 
++" made the emeralds " + verb + " in a wonderful manner.";
+
+Console.WriteLine("Here is your final story...");
+Console.WriteLine(story);
